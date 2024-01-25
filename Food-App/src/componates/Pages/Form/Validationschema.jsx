b05aba6d56a75be298ee74726b26f61a916fa764
haresh <hareschaudhary34@gmail.com>
2024-01-25 14:23:24 +0530

    food app

--- Food-App/src/componates/Pages/Form/Validationschema.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as Yup from "yup"
-const Validationschema=Yup.object({
-name: Yup.string().min(2).max(20).required("enter your name"),
-email: Yup.string().email().required("enter your email"),
-password: Yup.string().min(5).required("enter your password")
-})
-
-export default Validationschema
--- Food-App/src/contaxtapi/Usecontaxt.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react';
-
-const Usecontaxt =createContext()
-
-export default Usecontaxt
